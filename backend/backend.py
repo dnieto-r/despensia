7d4de2c06be971822b7a869c5b6a20c173ac831e
sergioqae <sergioqae@gmail.com>
2024-06-20 20:09:35 +0200

    Integrar imagen en puerto principal

--- backend/backend.py
+++ backend/backend.py
@@ -4,7 +4,8 @@ import openai
 import requests
 import json
 import argparse
-
+import base64
+from openai import AzureOpenAI
 
 app = Flask(__name__)
 
@@ -25,6 +26,34 @@ AZURE_DEPLOYMENT_IMAGES = "dall-e"
 AZURE_VERSION = "2024-02-01"
 AZURE_ENDPOINT = f"https://{AZURE_DOMAIN}.openai.azure.com/openai/deployments/{AZURE_DEPLOYMENT}/chat/completions?api-version={AZURE_VERSION}"
 AZURE_ENDPOINT_IMAGES = f"https://{AZURE_DOMAIN_IMAGES}.openai.azure.com/openai/deployments/{AZURE_DEPLOYMENT_IMAGES}/images/generations?api-version={AZURE_VERSION}"
+
+def image_azure_openai(base64_image):
+    client = AzureOpenAI(
+        api_key=AZURE_OPENAI_KEY,
+        azure_endpoint=AZURE_ENDPOINT,
+        api_version=AZURE_VERSION
+    )
+    response = client.chat.completions.create(
+        model=AZURE_DEPLOYMENT,
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": '¿Qué ingredientes hay en ésta imagen? Necesito que el formato de la respuesta venga en un json con el siguiente formato : {"ingredientes": ["tomate", "arroz"]}'
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{base64_image}"
+                        }
+                    }
+                ]
+            }
+        ]
+    )
+    return response.choices[0].message.content
 
 def consultar_azure_openai(prompt, endpoint, api_key, mode='text'):
     headers = {
@@ -91,6 +120,20 @@ def generate_prompt(ingredientes, equipamiento, perfil, comensales, dificultad, 
     
     return prompt
     
+@app.route('/imagen', methods=['POST'])
+def image_recognition():
+    if request.files.get("image") is None:
+        message = {'error': 'Missing image in multipart form data'}
+        return jsonify(message), 400
+    f = request.files["image"]
+    base64_image = base64.b64encode(f.read()).decode('utf-8')
+    message = image_azure_openai(base64_image)
+    response = app.response_class(
+        response= message.replace('```json\n', '').replace('```', ''),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 @app.route('/generar', methods=['POST'])
 def generar_receta():
