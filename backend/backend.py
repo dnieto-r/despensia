dc654c94164f0ef451411ba948c769b7816c4dd1
Raúl Pastor Sobrado <raul.pastorsobrado@telefonica.com>
2024-06-20 14:17:12 +0200

    feat: backend_improvement

--- backend/backend.py
+++ backend/backend.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import os
 import openai
+from openai import AzureOpenAI
 import requests
 import json
 
@@ -19,24 +20,21 @@ AZURE_DEPLOYMENT = "gpt-4o"
 AZURE_VERSION = "2024-02-01"
 AZURE_ENDPOINT = f"https://{AZURE_DOMAIN}.openai.azure.com/openai/deployments/{AZURE_DEPLOYMENT}/chat/completions?api-version={AZURE_VERSION}"
 
+
 def consultar_azure_openai(prompt):
-    headers = {
-        "Content-Type": "application/json",
-        "api-key": AZURE_OPENAI_KEY
-    }    
-    payload = {
-        "messages": [
-            {"role": "system", "content": "You are an AI assistant that helps people find information."},
-            {"role": "user", "content": prompt}
+    client = AzureOpenAI(
+        api_key=AZURE_OPENAI_KEY,
+        azure_endpoint=AZURE_ENDPOINT,
+        api_version=AZURE_VERSION
+    )
+    response = client.chat.completions.create(
+        model=AZURE_DEPLOYMENT,
+        messages=[
+            {"role": "system", "content": "Eres un asistente virtual que proporciona recetas de cocina."},
+            {"role": "user", "content": prompt},
         ]
-    }
-    response = requests.post(AZURE_ENDPOINT, headers=headers, json=payload)
-    if response.status_code == 200:
-        response_data = response.json()
-        respuesta_generada = response_data['choices'][0]['message']['content']
-        return respuesta_generada
-    else:
-        raise Exception(f"Error en la solicitud a Azure OpenAI: {response.status_code}, {response.text}")
+    )
+    return response.choices[0].message.content
 
 
 @app.route('/recetas', methods=['POST'])
@@ -46,11 +44,16 @@ def agregar_receta():
 
     for dato in datos_necesarios: 
         if dato not in datos_receta:
-            mensaje = {'error': 'Faltan datos requeridos'}
+            mensaje = {'error': 'Faltan datos requeridos, ' + dato}
             return jsonify(mensaje), 400
     prompt = 'Mi perfil como cocinero es el siguiente: la cocina es una pasión para mí. Me gusta utilizar técnicas avanzadas de cocina e innovar tanto en presentación como en fusión de ingredientes. Tengo los siguientes ingredientes en la nevera: salmón, peras, champiñones y espinacas. Tengo los siguientes utensilios de cocina disponibles: sartenes, ollas, horno, batidora, olla a presion, microhondas, freidora de aire, licuadora, soplete, mandolina, termometro, balanza. Estoy buscando una receta para 2 personas que me suponga un reto y que se pueda hacer en unos 90 minutos. Necesito que el formato de la respuesta venga en un json con el siguiente formato (el campo instrucciones puede estar dividido internamente en varios grupos por simplicidad si es necesario:{"titulo": "","descripcion": "","ingredientes": [],"utensilios": [],"instrucciones": ["paso_1": {"titulo": "","instrucciones": []},...]}'
     receta = consultar_azure_openai(prompt)
-    return jsonify(receta), 201
+    response = app.response_class(
+        response=receta.replace('```json\n', '').replace('```', ''),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 @app.route('/recetas', methods=['GET'])
 def obtener_recetas():
